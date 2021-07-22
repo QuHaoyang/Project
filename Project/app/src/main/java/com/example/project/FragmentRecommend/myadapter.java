1169c7ff7f7568a795dfce7b146745847c7bb932
QuHaoyang <1115131457@qq.com>
2021-07-22 23:10:38 +0800

    Project

--- Project/app/src/main/java/com/example/project/FragmentRecommend/myadapter.java
+++ Project/app/src/main/java/com/example/project/FragmentRecommend/myadapter.java
@@ -80,6 +80,7 @@ public class myadapter extends RecyclerView.Adapter <myadapter.MyViewHolder> {
      //   holder.cover.setImageURI(Uri.parse( video.getImageUrl()));
      //   holder.videoView.setVideoURI(Uri.parse( video.getVideoUrl()));
         holder.videoView.setUp(video.getVideoUrl(),null);
+        holder.videoView.posterImageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
         Glide.with(context).asDrawable().load(video.getImageUrl()).into(holder.videoView.posterImageView);
 //        holder.videoView.startVideo();
     }
